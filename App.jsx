34dea47ffc3d4038a103b97cc796082c3558eb73
Lucy Holmes <lucyholmes08@hotmail.com>
2024-04-23 15:57:12 +0100

    conditional rendering

--- App.jsx
+++ App.jsx
@@ -5,6 +5,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function App() {
+  //items list
+  const [items, setItems] = useState([
+    { id: 0, title: "Turnip" },
+    { id: 1, title: "Artichoke" },
+  ]);
+  //set state of button to false
+  const [isVisible, setIsVisible] = useState(false);
   // state set to 0
   const [myCounterState, setMyCounterState] = useState(0);
 
@@ -20,12 +27,27 @@ export default function App() {
 
   return (
     <div>
+      {/* header component */}
       <Header />
+      {/* button with toggle */}
+      <button onClick={() => setIsVisible(!isVisible)}>Toggle</button>
+      {isVisible ? <p>True</p> : <p>False</p>}
+      {/* items list */}
+      <h1>Items</h1>
+      <ul>
+        {items.length ? (
+          items.map((item) => <li key={item.id}>{item.title}</li>)
+        ) : (
+          <li>No items</li> //this line makes it so '0' doesnt appear on the page if there are no array items
+        )}
+      </ul>
+      {/* counter button component */}
       <Counter
         count={myCounterState}
         increment={incrementCounter}
         reset={resetCounter}
       />
+      {/* page content */}
       <h2>Hello, world!</h2>
       <p>This is some text</p>
       <p>This text will update on the server without having to refresh</p>
@@ -33,6 +55,7 @@ export default function App() {
         src="https://koala.sh/api/image/v2-4sy7w-wrf7f.jpg?width=1024&height=1024&dream"
         alt="calico cat"
       />
+      {/* footer component */}
       <Footer />
     </div>
   );
